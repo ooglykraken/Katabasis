06d3df7873369bc8bb393eaffd55100272c01914
Ian Ford <ooglykraken@hotmail.com>
2019-08-12 20:20:31 -0700

    initialize repo

--- Katabasis/Assets/Scripts/Misc/Key.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Key : MonoBehaviour {
-
-	public void FixedUpdate(){
-		GetComponent<Rigidbody>().velocity = Vector3.Lerp(GetComponent<Rigidbody>().velocity, Vector3.zero, Time.deltaTime * 6f);
-	}
-}
--- Katabasis/Assets/Scripts/UI/PopupButton.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class PopupButton : MonoBehaviour {
-	
-	public void Awake(){
-		gameObject.GetComponent<Button>().downTarget = transform.parent.gameObject;
-	}
-}
